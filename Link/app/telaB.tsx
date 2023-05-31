e213f6a84bd4f3350863263b558ce72d11e77cbf
Raquel <100868145+raquelm16@users.noreply.github.com>
2023-05-31 09:20:42 -0300

    Add files via upload

---
+++ Link/app/telaB.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {Link} from "expo-router"
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Tela B</Text>
+      <Link href="/">Ir para tela Home</Link>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulo:{
+    fontSize:24,
+    fontWeight:'bold'
+  }
+});
